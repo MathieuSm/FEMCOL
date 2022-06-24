3bdf062f80fc45e273f910d20db6df6aac750cf6
stedabra <stefan.bracher@hotmail.com>
2022-06-24 11:19:43 +0200

    First test in python

---
+++ 03_Scripts/read_MTS.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import os
+from scipy.signal import butter,filtfilt
+from scipy.signal import find_peaks
+from scipy import stats
+
+def butter_lowpass_filter(data, cutoff, order=9):
+    normal_cutoff = cutoff / nyq
+    # Get the filter coefficients
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
+
+
+parent_dir = '/home/stefan/Documents/PythonScripts/02_Data/02_MTS/Elastic_testing_mineralized/'
+filename_list = os.listdir(parent_dir)
+
+result_dir = {}
+for filename in filename_list:
+    sample_ID = filename.split('/')[-1].split('_')[1]
+    # load csv:
+    df = pd.read_csv(filename, skiprows=2)
+    df.rename(columns={'sec': 'time', 'N': 'force_MTS', 'N.1': 'force_lc', 'mm': 'disp_MTS', 'mm.1': 'disp_ext'},
+              inplace=True)
+
+    # peak detection:
+    peaks_index, _ = find_peaks(df['force_MTS'], width=50)
+
+    # linear regression:
+    slope, intercept, r_value, p_value, std_err = stats.linregress(df['disp_ext'][peaks_index[-1]:], df['force_MTS'][peaks_index[-1]:])
+    x_last_cycle = np.array([df.iloc[-1]['disp_ext'], df['disp_ext'][peaks_index[-1]]])
+
+
+    plt.figure(figsize=(6, 4))
+    plt.title(sample_ID)
+    plt.plot(df['disp_ext'], df['force_MTS'], label='MTS')
+    plt.plot(df['disp_ext'][peaks_index[-1]:], df['force_MTS'][peaks_index[-1]:])
+    plt.plot(x_last_cycle, slope * x_last_cycle + intercept, 'k')
+    plt.plot([], ' ', label=f'stiffness = {slope:.1f} N/mm')
+    plt.ylabel('force / N')
+    plt.xlabel('disp / mm')
+    plt.legend()
+    plt.show()
+    # plt.savefig('', dpi=300)
+
+
+    plt.plot(df['time'][peaks_index], df['force_MTS'][peaks_index], 'o')
+    plt.show()
+
+    # filter signals:
+    fs = 102.4       # sample rate, Hz
+    cutoff = 10
+    nyq = 0.5 * fs
+    df['force_lc_filtered'] = butter_lowpass_filter(df['force_lc'], cutoff)
+
+    plt.figure()
+    plt.plot(df['disp_ext'], df['force_lc'], label='MTS')
+    plt.plot(df['disp_ext'], df['force_lc_filtered'], label='MTS')
+    plt.ylabel('force / N')
+    plt.xlabel('displacement ext / mm')
+    plt.legend()
+    plt.show()
+
+
+    result_dir[sample_ID]['stiffness'] = slope
