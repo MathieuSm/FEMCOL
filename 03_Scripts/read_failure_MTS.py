5b089b321a296fcf252f748fca004cdc84b6eddc
stedabra <stefan.bracher@hotmail.com>
2022-07-06 13:05:59 +0200

    miscellaneous

--- 03_Scripts/read_failure_MTS.py
+++ 03_Scripts/read_failure_MTS.py
@@ -22,6 +22,7 @@ filename_list = [File for File in os.listdir(DataPath) if File.endswith('.csv')]
 filename_list.sort()
 
 i = 0
+result = list()
 
 for filename in filename_list:
     sample_ID = filename.split('/')[-1].split('_')[1]
@@ -35,52 +36,59 @@ for filename in filename_list:
     fs = 102.4  # sample rate, Hz
     cutoff = 5
     nyq = 0.5 * fs
-    df['force_MTS_filtered'] = butter_lowpass_filter(df['force_MTS'], cutoff)
+    df['force_lc_filtered'] = butter_lowpass_filter(df['force_lc'], cutoff)
 
     # plot filtered and raw signal both at a time & save with respective sample_ID as name
-    plt.figure()
-    plt.title(sample_ID)
-    plt.plot(df['disp_ext'], df['force_MTS'], label='MTS raw')
-    plt.plot(df['disp_ext'], df['force_MTS_filtered'], label='MTS filtered')
-    plt.ylabel('force MTS / N')
-    plt.xlabel('disp ext / mm')
-    plt.legend()
-    savepath = Cwd / '02_Data/02_MTS/'
-    plt.savefig(os.path.join(savepath, 'failure_' + sample_ID + '.png'), dpi=300)
+    # plt.figure()
+    # plt.title(sample_ID)
+    # plt.plot(df['disp_ext'], df['force_lc'], label='raw')
+    # plt.plot(df['disp_ext'], df['force_lc_filtered'], label='filtered')
+    # plt.ylabel('force lc / N')
+    # plt.xlabel('disp ext / mm')
+    # plt.legend()
+    # plt.close()
 
     # calculate stress and strain, filter & put into dataframe
     Pi = 3.1415
     area = 2*2*Pi/4
     l_initial = 6.5
-    stress = df['force_MTS']/area
+    stress = df['force_lc']/area
     strain = df['disp_ext']/l_initial
-    df['stress_MTS'] = stress
+    df['stress_lc'] = stress
     df['strain_ext'] = strain
-    df['stress_MTS_filtered'] = butter_lowpass_filter(df['stress_MTS'], cutoff)
+    df['stress_lc_filtered'] = butter_lowpass_filter(df['stress_lc'], cutoff)
 
     # plot stress vs strain raw/filtered
     plt.figure()
     plt.title(sample_ID)
-    plt.plot(df['strain_ext'], df['stress_MTS'], label='raw')
-    plt.plot(df['strain_ext'], df['stress_MTS_filtered'], label='filtered')
+    plt.plot(df['strain_ext'], df['stress_lc'], label='raw')
+    plt.plot(df['strain_ext'], df['stress_lc_filtered'], label='filtered')
     plt.ylabel('stress / MPa')
     plt.xlabel('strain / -')
     plt.legend()
-    plt.show()
+    savepath = Cwd / '02_Data/02_MTS/'
+    plt.savefig(os.path.join(savepath, 'failure_' + sample_ID + '.png'), dpi=300)
 
-# find max value of stress_MTS column
-column = df['stress_MTS']
-max_stress_raw = column.max()
-print(max_stress_raw)
+    # find max value of stress_MTS column
+    column = df['stress_lc']
+    max_stress_raw = column.max()
 
-# allocate corresponding index
-ind = df.index
-condition = df['stress_MTS'] == 'max_stress_raw'
-max_stress_raw_ind = ind[condition]
-max_stress_raw_ind_list = max_stress_raw_ind.tolist()
-print(max_stress_raw_ind_list)
+    # locate index of max value
+    row_max_stress = df.loc[df['stress_lc'] == max_stress_raw]
+    max_stress_index = row_max_stress.index
 
-# row = df.loc[df['stress_MTS'] == max_stress_raw]
-# row = df.index[df['stress_MTS'] == max_stress_raw]
-# index = row.iloc[0]
-# print(index)
+    # search for ultimate stress (filtered)
+    row_max_stress_filtered = df.iloc[max_stress_index]
+    # ultimate_stress_filtered = row_max_stress_filtered['stress_lc_filtered']
+    # returns only number as type 'array'
+    ultimate_stress_filtered = np.round(row_max_stress_filtered['stress_lc_filtered'].values, 2)
+
+    # search for ultimate strain
+    row_max_strain = df.iloc[max_stress_index]
+    ultimate_strain = np.round(row_max_strain['strain_ext'].values, 4)
+
+    values = [sample_ID, ultimate_stress_filtered, ultimate_strain]
+    result.append(values)
+    result_dir = pd.DataFrame(result, columns=['Sample ID', 'Ultimate stress MPa', 'Ultimate strain -'])
+
+print(result_dir)
