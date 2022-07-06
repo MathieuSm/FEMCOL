d272a4f196e14be634d7d3c3d214fdf89410b8f5
stedabra <stefan.bracher@hotmail.com>
2022-07-06 16:33:38 +0200

    create output file

--- 03_Scripts/gravimetry.py
+++ 03_Scripts/gravimetry.py
@@ -21,9 +21,30 @@ H20_density = 0.99754
 
 # calculate density with density=wet weight*density of H2O/(wet weight-H2O weight)
 for row in range(len(df)):
-    density = df['wet weight']*H20_density/(df['wet weight']-df['H2O weight'])
-Sample_ID = df['Sample ID']
-results = [Sample_ID, density]
-result_dir = pd.DataFrame(results, columns=['Sample ID','density g/cm^3'])
+    density = df['wet weight'] * H20_density / (df['wet weight'] - df['H2O weight'])
+    w_organic = df['dry weight'] - df['ash weight']
+    w_mineral = df['ash weight']
+    w_water = df['wet weight'] - df['dry weight']
+    wf_mineral = df['ash weight'] / df['wet weight']
+    wf_organic = w_organic / df['wet weight']
+    wf_water = 1 - wf_organic - wf_mineral
+
+#
+result_dir = pd.DataFrame()
+result_dir['Sample ID'] = df['Sample ID']
+result_dir['density / g/cm^3'] = density
+result_dir['organic weight / g'] = w_organic
+result_dir['mineral weight / g'] = w_mineral
+result_dir['water weight / g'] = w_water
+result_dir['weight fraction of mineral phase / -'] = wf_mineral
+result_dir['weight fraction of organic phase / -'] = wf_organic
+result_dir['weight fraction of water phase / -'] = wf_water
+
+# results = [Sample_ID, density]
+# result_dir = pd.DataFrame(results, columns=['Sample ID','density g/cm^3'])
+
+result_dir.to_csv(os.path.join('/home/stefan/Documents/PythonScripts/04_Results/02_Gravimetry/', 'ResultsGravimetry.csv'), index=False)
 
 print(result_dir)
+
+
