f6d43dc0d5d3e6737ccccd4f6265f325cd5b61eb
stedabra <stefan.bracher@hotmail.com>
2022-07-21 12:41:38 +0200

    adjust window width & plotting

--- 03_Scripts/read_MTS.py
+++ 03_Scripts/read_MTS.py
@@ -53,8 +53,6 @@ for filename in filename_list:
     # peak detection
     peaks_index, _ = find_peaks(df['force_lc'], width=50)
 
-    window_width = round(1 / 3 * (df.index[-1] - peaks_index[-1]))
-
     # calculate values for last cycle of force/disp curve only
     last_cycle_force = df['force_lc_filtered'][peaks_index[-1]:]
     last_cycle_disp = df['disp_ext'][peaks_index[-1]:]
@@ -77,22 +75,36 @@ for filename in filename_list:
     min_disp_ind = max(lower_cond_disp.index)
     last_cycle = last_cycle[max_disp_ind:min_disp_ind]
 
-    # rolling linear regression
+    window_width = round(1 / 3 * (last_cycle.index[-1]))
+
+    # rolling linear regression for stiffness calculation
     slope_values_stiff = list()
+    intercept_values_stiff = list()
     for x in range(0, len(last_cycle) - 1 - window_width + 1, 1):
         last_cycle_mod = last_cycle[x:x + window_width]
         slope_stiff, intercept_stiff, r_value, p_value, std_err = stats.linregress(last_cycle_mod['last_cycle_disp'],
                                                                        last_cycle_mod['last_cycle_force'])
-        slope_value = slope_stiff
-        slope_values_stiff.append(slope_value)
-    stiffness = max(slope_values_stiff)
+        slope_value_stiff = slope_stiff
+        intercept_value = intercept_stiff
+        slope_values_stiff.append(slope_value_stiff)
+        intercept_values_stiff.append(intercept_value)
+    slope_value_df = pd.DataFrame(slope_values_stiff)
+    intercept_values_df = pd.DataFrame(intercept_values_stiff)
+    stiffness = slope_value_df.max()[0]
+    max_slope_index = slope_value_df[slope_value_df == stiffness].dropna()
+    max_slope_index = max_slope_index.index
+    intercept_value_max_stiff = intercept_values_df.loc[max_slope_index[0]].values[0]
+    last_cycle_plot = last_cycle[max_slope_index[0]:max_slope_index[0] + window_width]
 
     # generate plot
     plt.figure(figsize=(6, 4))
     plt.title(sample_ID)
-    plt.plot(df['disp_ext'], df['force_lc'], label='lc')
-    plt.plot(df['disp_ext'][peaks_index[-1]:], df['force_lc'][peaks_index[-1]:])
-    plt.plot(last_cycle_disp, stiffness * last_cycle_disp + intercept_stiff, 'k')
+    plt.plot(df['disp_ext'], df['force_lc_filtered'], label='filtered')
+    plt.plot(last_cycle['last_cycle_disp'], last_cycle['last_cycle_force'], label='last unloading cycle 0.005-0.015')
+    plt.plot(last_cycle_plot['last_cycle_disp'], last_cycle_plot['last_cycle_force'], label='regress area',
+             color='k')
+    plt.plot(last_cycle_plot['last_cycle_disp'], stiffness * last_cycle_plot['last_cycle_disp'] +
+             intercept_value_max_stiff, 'r')
     plt.plot([], ' ', label=f'stiffness = {stiffness:.0f} N/mm')
     plt.ylabel('force lc / N')
     plt.xlabel('disp ext / mm')
@@ -100,7 +112,7 @@ for filename in filename_list:
     savepath = Cwd / '04_Results/00_Mineralized/00_force_disp/'
     plt.savefig(os.path.join(savepath, 'force_disp_' + sample_ID + '.png'), dpi=300)
     plt.show()
-    plt.close()
+    # plt.close()
 
     # calculate stress/strain
     Pi = 3.1415
@@ -136,7 +148,8 @@ for filename in filename_list:
     plt.plot(last_cycle_strain, last_cycle_stress)
     plt.ylabel('stress / MPa')
     plt.xlabel('strain / -')
-    plt.show()
+    # plt.show()
+    plt.close()
 
     # calculate apparent modulus of elasticity using regression
     slope, intercept, r_value, p_value, std_err = stats.linregress(last_cycle_strain, last_cycle_stress)
@@ -149,8 +162,8 @@ for filename in filename_list:
 
     # isolate stress/strain values of defined strain region
     last_cycle = pd.DataFrame()
-    last_cycle['last_cycle_strain'] = round(last_cycle_strain,5)
-    last_cycle['last_cycle_stress'] = round(last_cycle_stress,5)
+    last_cycle['last_cycle_strain'] = round(last_cycle_strain, 5)
+    last_cycle['last_cycle_stress'] = round(last_cycle_stress, 5)
 
     # identify index range of defined region
     upper_cond = last_cycle.loc[last_cycle['last_cycle_strain'] == upper_strain]
@@ -159,23 +172,50 @@ for filename in filename_list:
     min_strain_ind = min(lower_cond.index)
     last_cycle = last_cycle[max_strain_ind:min_strain_ind]
 
-    window_width = 358      # calculated by: index(lower_strain)-index(upper_strain)
     slope_values_app = list()
+    intercept_values_app = list()
 
-    # rolling linear regression
+    # rolling linear regression for apparent modulus calculation
     for x in range(max_strain_ind, min_strain_ind - window_width + 1, 1):
         last_cycle_mod_app = last_cycle[x:x+window_width]
-        slope_app, intercept, r_value, p_value, std_err = stats.linregress(last_cycle_mod_app['last_cycle_strain'],
+        slope_app, intercept_app, r_value, p_value, std_err = stats.linregress(last_cycle_mod_app['last_cycle_strain'],
                                                                        last_cycle_mod_app['last_cycle_stress'])
-        slope_value = slope_app
-        slope_values_app.append(slope_value)
-    apparent_modulus = max(slope_values_app)
+        slope_value_app = slope_app
+        slope_values_app.append(slope_value_app)
+        intercept_value_app = intercept_app
+        intercept_values_app.append(intercept_value_app)
+    slope_value_app_df = pd.DataFrame(slope_values_app)
+    intercept_values_app_df = pd.DataFrame(intercept_values_app)
+    apparent_modulus = slope_value_app_df.max()[0]
+    max_slope_index_app = slope_value_app_df[slope_value_app_df == apparent_modulus].dropna()
+    max_slope_index_app = max_slope_index_app.index
+    intercept_value_max_app = intercept_values_app_df.loc[max_slope_index_app[0]].values[0]
+    last_cycle_plot = last_cycle[max_slope_index_app[0]:max_slope_index_app[0] + window_width]
 
     # create list with current values which are sample_ID, slope & apparent modulus & add them to result list which
     # is then converted to dataframe
     values = [sample_ID, round(stiffness), round(apparent_modulus)]
     result.append(values)
     result_dir = pd.DataFrame(result, columns=['Sample ID', 'Stiffness N/mm', 'Apparent modulus MPa'])
+
+    # generate plot
+    plt.figure(figsize=(6, 4))
+    plt.title(sample_ID)
+    plt.plot(df['strain_ext'], df['stress_lc_filtered'], label='filtered')
+    plt.plot(last_cycle['last_cycle_strain'], last_cycle['last_cycle_stress'], label='last unloading cycle 0.001-0.0025')
+    plt.plot(last_cycle_plot['last_cycle_strain'], last_cycle_plot['last_cycle_stress'], label='regress area',
+             color='k')
+    plt.plot(last_cycle_plot['last_cycle_strain'], apparent_modulus * last_cycle_plot['last_cycle_strain'] +
+             intercept_value_max_app, 'r')
+    plt.plot([], ' ', label=f'apparent modulus = {apparent_modulus:.0f} MPa')
+    plt.ylabel('stress / MPa')
+    plt.xlabel('strain / -')
+    plt.legend()
+    savepath = Cwd / '04_Results/00_Mineralized/01_stress_strain/'
+    plt.savefig(os.path.join(savepath, 'stress_strain_' + sample_ID + '.png'), dpi=300)
+    plt.show()
+    # plt.close()
+
 
 missing_sample_IDs = pd.DataFrame({'Sample ID': ['390R', '395R', '400R', '402L', '433L']})
 # missing_IDs = pd.DataFrame(missing_sample_IDs, columns=['Sample ID'])
