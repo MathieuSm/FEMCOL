c90c8ed2fc75258daa1c014b652adf8535891af5
stedabra <stefan.bracher@hotmail.com>
2023-01-20 12:44:09 +0100

    added uFE data of Benjamin

--- 03_Scripts/results_merged.py
+++ 03_Scripts/results_merged.py
@@ -14,38 +14,42 @@ results_demineralized = pd.read_csv(str(Cwd / '04_Results/01_Demineralized/Resul
 results_gravimetry = pd.read_csv(str(Cwd / '04_Results/02_Gravimetry/ResultsGravimetry.csv'), skiprows=0)
 results_uCT = pd.read_csv(str(Cwd / '04_Results/03_uCT/ResultsUCT.csv'), skiprows=0)
 results_raman = pd.read_csv(str(Cwd / '04_Results/05_Raman/RamanResults.csv'), skiprows=0)
+results_uFE = pd.read_csv(str(Cwd / '02_Data/04_microFE/uFE_Data_Benjamin.csv'), skiprows=0)
 
 results_merged = pd.DataFrame()
 results_merged['Sample ID'] = results_mineralized['Sample ID']
-results_merged['Age'] = results_gravimetry['age']
+results_merged['Age / y'] = results_gravimetry['age']
 results_merged['Gender'] = results_gravimetry['sex']
 results_merged['Site'] = results_gravimetry['site']
-results_merged['Stiffness Mineralized N/mm'] = results_mineralized['Stiffness N/mm']
-results_merged['Apparent Modulus Mineralized MPa'] = results_mineralized['Apparent modulus MPa']
-results_merged['Ultimate Force N'] = results_demineralized['Ultimate Force / N']
-results_merged['Ultimate Stress MPa'] = results_demineralized['Ultimate stress / MPa']
-results_merged['Ultimate Strain -'] = results_demineralized['Ultimate strain / -']
-results_merged['Stiffness Demineralized N/mm'] = results_demineralized['Stiffness / N/mm']
-results_merged['Apparent Modulus Demineralized MPa'] = results_demineralized['Apparent modulus / MPa']
-results_merged['Density g/' + 'cm\u00B3'] = results_gravimetry['density / g/cm^3']
-results_merged['Organic Weight g'] = results_gravimetry['organic weight / g']
-results_merged['Mineral Weight g'] = results_gravimetry['mineral weight / g']
-results_merged['Water Weight g'] = results_gravimetry['water weight / g']
-results_merged['Mineral weight fraction -'] = results_gravimetry['weight fraction of mineral phase / -']
-results_merged['Organic weight fraction -'] = results_gravimetry['weight fraction of organic phase / -']
-results_merged['Water weight fraction -'] = results_gravimetry['weight fraction of water phase / -']
-results_merged['Bone Volume Fraction -'] = results_uCT['Bone Volume Fraction -']
-results_merged['Bone Mineral Density mg HA / ' + 'cm\u00B3'] = results_uCT['Bone Mineral Density mg HA / cm3']
-results_merged['Tissue Mineral Density mg HA / ' + 'cm\u00B3'] = results_uCT['Tissue Mineral Density mg HA / cm3']
-results_merged['Bone Mineral Content mg HA'] = results_uCT['Bone Mineral Content mg HA']
-results_merged['Minimum Area ' + 'mm\u00B2'] = results_uCT['Min Area mm^2']
-results_merged['Mean Apparent Area ' + 'mm\u00B2'] = results_uCT['Mean Apparent Area mm^2']
-results_merged['Minimum Equivalent Diameter mm'] = results_uCT['Min Diameter mm']
-results_merged['Mean Apparent Diameter mm'] = results_uCT['Mean Apparent Diameter mm']
-results_merged['Mean Area Fraction -'] = results_uCT['Mean Area Fraction -']
-results_merged['Min Area Fraction -'] = results_uCT['Min Area Fraction -']
-results_merged['Mineral to Matrix Ratio -'] = results_raman['M2M_ratio_mean']
-results_merged.to_csv(os.path.join('/home/stefan/Documents/FEMCOL/04_Results/', 'ResultsOverview.csv'),
+results_merged['Stiffness Mineralized / N/mm'] = results_mineralized['Stiffness N/mm']
+results_merged['Apparent Modulus Mineralized / MPa'] = results_mineralized['Apparent modulus MPa']
+results_merged['Ultimate Force / N'] = results_demineralized['Ultimate Force / N']
+results_merged['Ultimate Stress / MPa'] = results_demineralized['Ultimate stress / MPa']
+results_merged['Ultimate Strain / -'] = results_demineralized['Ultimate strain / -']
+results_merged['Stiffness Demineralized / N/mm'] = results_demineralized['Stiffness / N/mm']
+results_merged['Apparent Modulus Demineralized / MPa'] = results_demineralized['Apparent modulus / MPa']
+results_merged['Density / g/' + 'cm\u00B3'] = results_gravimetry['density / g/cm^3']
+results_merged['Organic Weight / g'] = results_gravimetry['organic weight / g']
+results_merged['Mineral Weight / g'] = results_gravimetry['mineral weight / g']
+results_merged['Water Weight / g'] = results_gravimetry['water weight / g']
+results_merged['Mineral weight fraction / -'] = results_gravimetry['weight fraction of mineral phase / -']
+results_merged['Organic weight fraction / -'] = results_gravimetry['weight fraction of organic phase / -']
+results_merged['Water weight fraction / -'] = results_gravimetry['weight fraction of water phase / -']
+results_merged['Bone Volume Fraction / -'] = results_uCT['Bone Volume Fraction -']
+results_merged['Bone Mineral Density / mg HA / ' + 'cm\u00B3'] = results_uCT['Bone Mineral Density mg HA / cm3']
+results_merged['Tissue Mineral Density / mg HA / ' + 'cm\u00B3'] = results_uCT['Tissue Mineral Density mg HA / cm3']
+results_merged['Bone Mineral Content / mg HA'] = results_uCT['Bone Mineral Content mg HA']
+results_merged['Minimum Area / ' + 'mm\u00B2'] = results_uCT['Min Area mm^2']
+results_merged['Mean Apparent Area / ' + 'mm\u00B2'] = results_uCT['Mean Apparent Area mm^2']
+results_merged['Minimum Equivalent Diameter / mm'] = results_uCT['Min Diameter mm']
+results_merged['Mean Apparent Diameter / mm'] = results_uCT['Mean Apparent Diameter mm']
+results_merged['Mean Area Fraction / -'] = results_uCT['Mean Area Fraction -']
+results_merged['Min Area Fraction / -'] = results_uCT['Min Area Fraction -']
+results_merged['Mineral to Matrix Ratio / -'] = results_raman['M2M_ratio_mean']
+results_merged['Apparent Modulus Mineralized uFE / MPa'] = round(results_uFE['E_uFE_L'], 3)*1000
+results_merged['Yield Strength uFE/ MPa'] = round(results_uFE['yield_strength_uFE_with_non_broken'], 2)
+results_merged['Ultimate Stress uFE/ MPa'] = round(results_uFE['strength_uFE_with_non_broken'], 2)
+results_merged.to_csv(os.path.join(Cwd / '04_Results/', 'ResultsOverview.csv'),
                       index=False)
 
 
