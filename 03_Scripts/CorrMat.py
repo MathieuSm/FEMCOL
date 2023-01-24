83f8e57266005db0b42e4a2e2f5a16a67dc7faa8
stedabra <stefan.bracher@hotmail.com>
2023-01-24 14:00:50 +0100

    changed various heatmap related things: added plot including r-values and asterisk indicating significance level (p-value), changed colorbar, changed cmap for r-values to cyclic

--- 03_Scripts/CorrMat.py
+++ 03_Scripts/CorrMat.py
@@ -28,35 +28,52 @@ df_new = df[['Bone Volume Fraction / -', 'Bone Mineral Density / mg HA / cm³', 
              'Water weight fraction / -', 'Density / g/cm³', 'Apparent Modulus Mineralized / MPa',
              'Apparent Modulus Demineralized / MPa', 'Ultimate Stress / MPa', 'Ultimate Strain / -',
              'Apparent Modulus Mineralized uFE / MPa', 'Yield Stress uFE / MPa', 'Ultimate Stress uFE / MPa']]
-df_new.columns = ['Bone Volume Fraction', 'Bone Mineral Density', 'Tissue Mineral Density', 'Mineral to Matrix Ratio',
-                  'Mineral Weight Fraction', 'Organic Weight Fraction', 'Water Weight Fraction', 'Bone Density',
-                  'Apparent Modulus Mineralized', 'Apparent Modulus Demineralized', 'Ultimate Stress', 'Ultimate Strain',
-                  'Apparent Modulus Mineralized uFE', 'Yield Stress uFE', 'Ultimate Stress uFE']
+# df_new.columns = ['Bone Volume Fraction', 'Bone Mineral Density', 'Tissue Mineral Density', 'Mineral to Matrix Ratio',
+#                   'Mineral Weight Fraction', 'Organic Weight Fraction', 'Water Weight Fraction', 'Bone Density',
+#                   'Apparent Modulus Mineralized', 'Apparent Modulus Demineralized', 'Ultimate Stress', 'Ultimate Strain',
+#                   'Apparent Modulus Mineralized uFE', 'Yield Stress uFE', 'Ultimate Stress uFE']
+df_new.columns = ['BVTV', 'BMD', 'TMD', 'MMR', 'WFM', 'WFO', 'WFW', 'D', 'AMM', 'AMD', 'USTRE', 'USTRA', 'AMMuFE',
+                  'YSTREuFE', 'USTREuFE']
 
 # Create empty dataframe for p-values and correlation matrix containing r values
 corr_matrix_p = pd.DataFrame()
-corr_matrix_r = round(df_new.corr(), 3)
+corr_matrix_r = round(df_new.corr(), 2)
+test_matrix_r = pd.DataFrame()
 
 # loop to iterate through rows and columns
 for i in df_new.columns:
     for j in df_new.columns:
-        # pvalue = round(linregress(df_new[j], df_new[i])[3], 3)
-        # if pvalue <= 0.001:
-        #     p = r'\textsuperscript{***}'
-        # elif pvalue <= 0.01:
-        #     p = r'\textsuperscript{**}'
-        # elif pvalue <= 0.05:
-        #     p = r'\textsuperscript{*}'
 
         # mask to select only values that are not NaN (linregress does not work otherwise)
         mask = ~df_new[i].isna() & ~df_new[j].isna()
+        slope, intercept, r, pvalue, std_err = linregress(df_new[j][mask], df_new[i][mask])
 
         # create matrix containing p-values using mask criterion
-        corr_matrix_p.loc[i, j] = round(linregress(df_new[j][mask], df_new[i][mask])[3], 3)
+        # corr_matrix_p.loc[i, j] = round(linregress(df_new[j][mask], df_new[i][mask])[3], 3)
+        corr_matrix_p.loc[i, j] = pvalue
 
-p_matrix = pd.DataFrame()
+        # pvalue = round(linregress(df_new[j][mask], df_new[i][mask])[3], 10)
+        if pvalue <= 0.001:
+            p = r'\textsuperscript{***}'
+            test_matrix_r.loc[i, j] = str(corr_matrix_r.loc[i, j]) + str(p)
+        elif pvalue <= 0.01:
+            p = r'\textsuperscript{**}'
+            test_matrix_r.loc[i, j] = str(corr_matrix_r.loc[i, j]) + str(p)
+        elif pvalue <= 0.05:
+            p = r'\textsuperscript{*}'
+            test_matrix_r.loc[i, j] = str(corr_matrix_r.loc[i, j]) + str(p)
+        elif pvalue > 0.05:
+            test_matrix_r.loc[i, j] = str(corr_matrix_r.loc[i, j])
+
+# Masking upper triangle to show only lower triangle for p-plot without asterisk
 mask_p = np.zeros_like(corr_matrix_p, dtype=np.bool_)
 mask_p[np.triu_indices_from(mask_p)] = True
+
+# Masking upper triangle to show only lower triangle for p-plot with asterisk
+mask_r_red = np.zeros_like(corr_matrix_r, dtype=np.bool_)
+mask_r_red[np.tril_indices_from(mask_r_red)] = True
+corr_matrix_r_red = (test_matrix_r.mask(mask_r_red)).fillna('')
+
 
 # Colormap trick for p-matrix
 viridis_p = cm.get_cmap('plasma', 4)
@@ -64,8 +81,12 @@ newcolors_p = viridis_p(np.linspace(0, 1, 4))
 newcmp_p = ListedColormap(newcolors_p)
 
 # Colormap trick for r-matrix
-viridis_r = cm.get_cmap('plasma', 8)
-newcolors_r = viridis_r(np.linspace(0, 1, 8))
+# viridis_r = cm.get_cmap('plasma', 8)
+# newcolors_r = viridis_r(np.linspace(0, 1, 8))
+# newcmp_r = ListedColormap(newcolors_r)
+
+twilight = cm.get_cmap('twilight', 8)
+newcolors_r = twilight(np.linspace(0, 1, 8))
 newcmp_r = ListedColormap(newcolors_r)
 
 # Axis annotations
@@ -91,15 +112,16 @@ heatmap_p = sns.heatmap(Trick,
                         linewidths=.5,
                         cmap=newcmp_p,
                         cbar_kws={'shrink': .5,
-                                  'ticks': np.array([0, 1/4, 2/4, 3/4, 1])},
+                                  'ticks': np.array([0, 1/4, 2/4, 3/4, 1]),
+                                  'label': 'p-value'},
                         vmin=0,
                         vmax=1,
+                        fmt='.2f',
                         annot=corr_matrix_p,
                         annot_kws={'size': 12})
 
 # set ticklabels of colorbar
 ax.collections[0].colorbar.set_ticklabels([0, 0.001, 0.01, 0.05, 1])
-
 
 #add column names as labels
 ax.set_yticklabels(abbreviations, rotation=0, fontsize=14)
@@ -123,10 +145,12 @@ heatmap_r = sns.heatmap(corr_matrix_r,
                         linewidths=.5,
                         cmap=newcmp_r,
                         cbar_kws={'shrink': .5,
-                                  'ticks': np.linspace(0, 1, 9)},
-                        vmin=0,
+                                  'ticks': np.linspace(-1, 1, 9),
+                                  'label': 'Pearson Correlation Coefficient (r)'},
+                        vmin=-1,
                         vmax=1,
                         annot=True,
+                        fmt='.2f',
                         annot_kws={'size': 12})
 
 #add the column names as labels
@@ -136,5 +160,36 @@ ax.set_xticklabels(abbreviations, fontsize=14)
 sns.set_style({'xtick.bottom': True}, {'ytick.left': True})
 plt.savefig(os.path.join(savepath, 'correlation_matrix_heatmap_rvalues.eps'), dpi=600, bbox_inches='tight', format='eps')
 plt.savefig(os.path.join(savepath, 'correlation_matrix_heatmap_rvalues.png'), dpi=600, bbox_inches='tight', format='png')
+
+plt.show()
+
+# Plotting of r-values containing p-value asterisk
+f, ax = plt.subplots(figsize=(11, 15))
+heatmap_r = sns.heatmap(corr_matrix_r,
+                        mask=mask_r,
+                        square=True,
+                        linewidths=.5,
+                        cmap=newcmp_r,
+                        cbar_kws={'shrink': .5,
+                                  'ticks': np.linspace(-1, 1, 9),
+                                  'label': 'Pearson Correlation Coefficient (r)'},
+                        vmin=-1,
+                        vmax=1,
+                        fmt='.2f',
+                        annot=False,
+                        annot_kws={'size': 12})
+
+for i, c in enumerate(corr_matrix_r_red.columns):
+    for j, v in enumerate(corr_matrix_r_red[c]):
+        ax.text(i + 0.5, j + 0.5, str(corr_matrix_r_red.iloc[i, j]), ha='center', va='center', color='w')
+
+#add column names as labels
+ax.set_yticklabels(abbreviations, rotation=0, fontsize=14)
+ax.set_xticklabels(abbreviations, fontsize=14)
+
+sns.set_style({'xtick.bottom': True}, {'ytick.left': True})
+
+plt.savefig(os.path.join(savepath, 'correlation_matrix_heatmap_rvalues_asterisk.eps'), dpi=600, bbox_inches='tight', format='eps')
+plt.savefig(os.path.join(savepath, 'correlation_matrix_heatmap_rvalues_asterisk.png'), dpi=600, bbox_inches='tight', format='png')
 
 plt.show()
