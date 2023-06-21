66ebf5835cc1067a3b8f78767e462ddcae79c3ae
stedabra <stefan.bracher@hotmail.com>
2023-06-21 11:19:08 +0200

    added variable inflation factor for multiple linear regression

--- 03_Scripts/MLR.py
+++ 03_Scripts/MLR.py
@@ -1,4 +1,5 @@
 import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 from pathlib import Path                            # Used to manage path variables in windows or linux
 import numpy as np                                  # Used to do arrays (matrices) computations namely
 import pandas as pd                                 # Used to manage data frames
@@ -22,7 +23,17 @@ df = df.dropna()
 df = df.reset_index(drop=True)
 
 x = df[['Age / y', 'Mineral weight fraction / -']]
-y = df['Modulus Mineralized / MPa']
+y = df[['Modulus Mineralized / MPa']]
+
+x_abbrev = x.copy()
+x_abbrev = x_abbrev.rename(columns={'Age / y': 'Age', 'Mineral weight fraction / -': 'mwf'})
+y_abbrev = y.copy()
+y_abbrev = y_abbrev.rename(columns={'Modulus Mineralized / MPa': 'Em'})
+data = pd.DataFrame()
+data['Em'] = y_abbrev['Em']
+data['Age'] = x_abbrev['Age']
+data['mwf'] = x_abbrev['mwf']
+
 
 # with statsmodels
 x = sm.add_constant(x) # adding a constant
@@ -36,3 +47,15 @@ print(print_model)
 mls_results = open('/home/stefan/PycharmProjects/FEMCOL/04_Results/04_Plots/mls_results.csv', 'w')
 n = mls_results.write(print_model.as_csv())
 mls_results.close()
+
+model_interaction = smf.ols(formula='Em ~ Age + mwf + Age:mwf', data=data).fit()
+summary_interaction = model_interaction.summary()
+print(summary_interaction)
+
+mls_results_interaction = open('/home/stefan/PycharmProjects/FEMCOL/04_Results/04_Plots/mls_results_interaction.csv', 'w')
+n = mls_results_interaction.write(summary_interaction.as_csv())
+mls_results_interaction.close()
+
+X = sm.add_constant(data)
+vif = pd.Series([variance_inflation_factor(X.values, i) for i in range(X.shape[1])], index=X.columns)
+print(vif)
